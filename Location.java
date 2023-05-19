3971b9cc10db07b1df3a47af0cbf49510a806e5b
Ajmel Muadz <muadzshakir26@gmail.com>
2023-05-19 19:36:25 +0800

    added setters and getters for Location class

--- Location.java
+++ Location.java
@@ -1,7 +1,40 @@
 public class Location 
 {
     // Class fields
-    private float latitude;
-    private float longitude;
+    private double latitude;
+    private double longitude;
     private String coordinateSystem;
+
+    // Accessor for latitude
+    public double getLatitude()
+    {
+        return latitude;
+    }
+    // Mutator for latitude
+    public void setLatitude(double pLatitude)
+    {
+        latitude = pLatitude;
+    }
+
+    // Accessor for longitude
+    public double getLongitude()
+    {
+        return longitude;
+    }
+    // Mutator for longitude
+    public void setLongitude(double pLongitude)
+    {
+        longitude = pLongitude;
+    }
+
+    // Accessor for coordinateSystem
+    public String getCoordinateSystem()
+    {
+        return coordinateSystem;
+    }
+    // Mutator for coordinateSystem
+    public void setCoordinateSystem(String pCoordinateSystem)
+    {
+        coordinateSystem = pCoordinateSystem;
+    }
 }
