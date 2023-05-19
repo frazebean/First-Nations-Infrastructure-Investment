ff76b1ae5631180ff1c3e854ddf2b190be4489bf
Ajmel Muadz <muadzshakir26@gmail.com>
2023-05-19 15:27:22 +0800

    completed bare modularisation and fixed statisticsMenuLoop bug

--- Menu.java
+++ Menu.java
@@ -6,24 +6,24 @@ public class Menu
     {
         Scanner input = new Scanner(System.in);
 
-        // menuOption array below to store options.
-        String[] menuOptions = {"All of Canada", "Alberta", "British Columbia", "Manitoba",
+        /* Array that stores provinces/territories. To be used if user selects a specific
+           province or territory in Canada. */
+        String[] provAndTerr = {"Alberta", "British Columbia", "Manitoba",
                                "New Brunswick", "Newfoundland and Labrador", "Nova Scotia",
                                "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan",
                                "Northwest Territories", "Nunavut", "Yukon"};
 
         int menuSelection, statisticsChoice;
-        boolean mainMenuLoop = true, statisticsMenuLoop = true;
+        boolean mainMenuLoop = true;
 
         System.out.println("Welcome to the Investments in Indigenous community " +
         "infrastructure\nProgram. There are a total of 'XYZ' projects throughout Canada.");
 
         while(mainMenuLoop)
         {
-            // Display the menu options.
-            System.out.println("Please make a selection from the Menu below.\n");
-            displayMenuOptions(menuOptions);
-            System.out.println("> 0.  Exit Program");
+            boolean statisticsMenuLoop = true;
+
+            displayMainMenu();
 
             try
             {
@@ -33,23 +33,16 @@ public class Menu
     
                 if(menuSelection == 1)
                 {
-                    System.out.println("\nThe total number of projects in Canada: XYZ");
-                    System.out.println("The total number of Ongoing projects: XYZ");
-                    System.out.println("The total number of projects Completed: XYZ");
-                    System.out.println("The percentage of Completed Projects: XYZ\n");
+                    displayAllOfCanadaInfo();
                 }
                 else if(menuSelection >= 2 && menuSelection <= 14)
                 {
                     while(statisticsMenuLoop)
                     {
                         System.out.println("\nPlease make a choice from the statistics below for " +
-                        menuOptions[menuSelection-1] + ":");
-                        System.out.println("> 1. Number of projects" +
-                                        "\n> 2. Percentage of all projects located in this province/territory" +
-                                        "\n> 3. Total number and percentage of Ongoing projects" +
-                                        "\n> 4. Total number and percentage of Completed projects" +
-                                        "\n> 5. All of the above statistics" +
-                                        "\n> 6. Return to main menu");
+                        provAndTerr[menuSelection-2] + ":");
+
+                        displayProvTerrMenu();
 
                         try
                         {
@@ -113,18 +106,41 @@ public class Menu
         }
     }
 
-    public static void displayMenuOptions(String[] pMenuOptions)
+    public static void displayMainMenu()
     {
-        for(int i = 0; i < pMenuOptions.length; i++)
-        {
-            if(i+1 >= 1 && i+1 <= 9)
-            {
-                System.out.println("> " + (i+1) + ".  " + pMenuOptions[i]);
-            }
-            else
-            {
-                System.out.println("> " + (i+1) + ". " + pMenuOptions[i]);
-            }
-        }
+        System.out.println("Please make a selection from the Menu below.\n" +
+        "\n> 1.  All of Canada" +
+        "\n> 2.  Alberta" +
+        "\n> 3.  British Columbia" +
+        "\n> 4.  Manitoba" +
+        "\n> 5.  New Brunswick" +
+        "\n> 6.  Newfoundland and Labrador" +
+        "\n> 7.  Nova Scotia" +
+        "\n> 8.  Ontario" +
+        "\n> 9.  Prince Edward Island" +
+        "\n> 10. Quebec" +
+        "\n> 11. Saskatchewan" +
+        "\n> 12. Northwest Territories" +
+        "\n> 13. Nunavut" +
+        "\n> 14. Yukon" +
+        "\n> 0.  Exit Program");
+    }
+
+    public static void displayAllOfCanadaInfo()
+    {
+        System.out.println("\nThe total number of projects in Canada: XYZ");
+        System.out.println("The total number of Ongoing projects: XYZ");
+        System.out.println("The total number of projects Completed: XYZ");
+        System.out.println("The percentage of Completed Projects: XYZ\n");
+    }
+
+    public static void displayProvTerrMenu()  // 'ProvTerr' is short form for 'Province/Territory'
+    {
+        System.out.println("> 1. Number of projects" +
+        "\n> 2. Percentage of all projects located in this province/territory" +
+        "\n> 3. Total number and percentage of Ongoing projects" +
+        "\n> 4. Total number and percentage of Completed projects" +
+        "\n> 5. All of the above statistics" +
+        "\n> 6. Return to main menu");
     }
 }
