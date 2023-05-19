06f06b4774fd276a181967447209759bddb574a0
Ajmel Muadz <muadzshakir26@gmail.com>
2023-05-19 18:58:57 +0800

    created instance variables for Project class

--- Project.java
+++ Project.java
@@ -1,4 +1,11 @@
 public class Project 
 {
-    
+    // Class fields
+    private String province;
+    private String beneficiary;
+    private String beneficiaryNum;
+    private String assetClass;
+    private String name;
+    private String stage;
+    private Location location;
 }
