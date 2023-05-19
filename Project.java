3b58b681c09060350844c5b9d893024020d82847
Ajmel Muadz <muadzshakir26@gmail.com>
2023-05-19 19:58:46 +0800

    added setters and getters for Project class

--- Project.java
+++ Project.java
@@ -8,4 +8,81 @@ public class Project
     private String name;
     private String stage;
     private Location location;
+
+    // Accessor for province
+    public String getProvince()
+    {
+        return province;
+    }
+    // Mutator for province
+    public void setProvince(String pProvince)
+    {
+        province = pProvince;
+    }
+
+    // Accessor for beneficiary
+    public String getBeneficiary()
+    {
+        return beneficiary;
+    }
+    // Mutator for beneficiary
+    public void setBeneficiary(String pBeneficiary)
+    {
+        beneficiary = pBeneficiary;
+    }
+
+    // Accessor for beneficiaryNum
+    public String getBeneficiaryNum()
+    {
+        return beneficiaryNum;
+    }
+    // Mutator for beneficiaryNum
+    public void setBeneficiaryNum(String pBeneficiaryNum)
+    {
+        beneficiaryNum = pBeneficiaryNum;
+    }
+
+    // Accessor for assetClass
+    public String getAssetClass()
+    {
+        return assetClass;
+    }
+    // Mutator for assetClass
+    public void setAssetClass(String pAssetClass)
+    {
+        assetClass = pAssetClass;
+    }
+
+    // Accessor for name
+    public String getName()
+    {
+        return name;
+    }
+    // Mutator for name
+    public void setName(String pName)
+    {
+        name = pName;
+    }
+
+    // Accessor for stage
+    public String getStage()
+    {
+        return stage;
+    }
+    // Mutator for stage
+    public void setStage(String pStage)
+    {
+        stage = pStage;
+    }
+
+    // Accessor for location
+    public Location getLocation()
+    {
+        return location;
+    }
+    // Mutator for location
+    public void setLocation(Location pLocation)
+    {
+        location = pLocation;
+    }
 }
