0203b67aacead254ede234105f4284927e9b7fa8
Ajmel Muadz <muadzshakir26@gmail.com>
2023-05-24 09:57:16 +0800

    revert changes

--- Menu.java
+++ Menu.java
@@ -6,8 +6,6 @@
  *          main program functionality.                                                     *
  ********************************************************************************************/
 
-// Change
-
 import java.util.*;
 import java.io.*;
 
